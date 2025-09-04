4397b4a3c7115da376042383187b3b9e46fd551d
Michael Galo <michael@michaelthinks.co>
2025-09-04 16:03:29 -0500

    adjusted chart bins

--- src/components/exoplanets/exoplanets-charts.js
+++ src/components/exoplanets/exoplanets-charts.js
@@ -1,7 +1,9 @@
+import { range } from "d3";
 import ExoplanetHistogram from "./exoplanet-histogram";
 
 const ExoplanetsCharts = ({allExoplanets}) => {
   const discoveryYears = allExoplanets.map((planet) => Number(planet.discovery_year));
+  const yearBins = Math.min(new Set(allExoplanets.map((planet) => planet.discovery_year)).size)
   const radii = allExoplanets.map((planet) => Number(planet.radius_earth_radii));
   const radiiBins = [0, 0.5, 1, 1.5, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 30, 40, 50, 75, 100, 150, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
   const radiiLabels = radiiBins.slice(1).map((upper, i) => {
@@ -11,7 +13,7 @@ const ExoplanetsCharts = ({allExoplanets}) => {
   const discoveryMethodLabels = [...new Set(allExoplanets.map((planet) => planet.discovery_method))];
   const discoveryMethods = allExoplanets.map((planet) => planet.discovery_method);
   const distance = allExoplanets.map((planet) => Number(planet.system_distance));
-  const distanceLabels = ["0-10", "10-20", "20-30", "30-40", "40-50", "50-60", "60-70", "70-80", "80-90", "90-100", ">100"];
+  const distanceLabels = ["0-10 parsecs", "10-20 parsecs", "20-30 parsecs", "30-40 parsecs", "40-50 parsecs", "50-60 parsecs", "60-70 parsecs", "70-80 parsecs", "80-90 parsecs", "90-100 parsecs", ">100 parsecs"];
 
 
   return (
@@ -20,7 +22,7 @@ const ExoplanetsCharts = ({allExoplanets}) => {
           <ExoplanetHistogram
             data={discoveryYears}
             height={500}
-            bins={Math.min(new Set(allExoplanets.map((planet) => planet.discovery_year)).size)}
+            bins={yearBins}
             title="Exoplanet Discoveries by Year"
             tickFormatSpec="d"
           />
@@ -31,7 +33,7 @@ const ExoplanetsCharts = ({allExoplanets}) => {
             data={distance}
             height={500}
             bins={Math.max(distance.length, 1)}
-            title="Distance to Host Star (in parsecs)"
+            title="Distance to Host Star"
             labels={distanceLabels}
             />
         </div>
